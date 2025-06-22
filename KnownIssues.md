9cc021a71ccebc0c8163bc9f5b7ef0e94acbef12
Michael Casella <mikec9490@gmail.com>
2025-06-21 21:55:58 -0400

    Conversation finished
    The thought bubbles have now been implemented. You can now correctly hover over the soldier's face and a thought bubble appears. Not only does it appear, but it looks clean - the text fits the bubble. Now I just need to hide both bubbles at the end and show the link that'll take you to the next page.

--- KnownIssues.md
+++ KnownIssues.md
@@ -1,5 +1,3 @@
 Known Issues so far:
 
 A) We need a signup implementation system. If the user is already signed up (i.e has an account) then when "Enlist now!" is clicked, instead of going to the signup page, they should go to the Main Page.
-
-B) Instead of the text just appearing in front of each soldier's head, they should appear in the thought bubble image. The process is as follows: if the user hovers over the thought bubble location, then the corresponding thought bubble appears (away from the bubble location, still towards the head) with its corresponding bubble text depending on the point in conversation. 
--- src/functionality/Welcome.js
+++ src/functionality/Welcome.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const leftSoldier = document.getElementById("leftSoldier");
+  const leftSoldier = document.getElementById("cursormarker1");
+  const rightSoldier = document.getElementById("cursormarker2");
   const bubbletext1 = document.getElementById("bubbletext1");
-  const rightSoldier = document.getElementById("rightSoldier");
   const bubbletext2 = document.getElementById("bubbletext2");
   const nextPageLink = document.getElementById("JourneyEmbarker");
+  const leftBubble = document.getElementById("leftBubble");
+  const rightBubble = document.getElementById("leftBubble");
 
   nextPageLink.style.display = "none";
 
@@ -14,11 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   leftSoldier.addEventListener("mouseenter", () => {
     if (pointInConversation === 0) {
-      bubbletext1.style.backgroundImage = "url('../images/thoughtBubble.jpg')";
       bubbletext1.textContent = "I sure wish I could remember the Roman Empire...";
       pointInConversation = 1;
     } else if (pointInConversation === 2) {
-      bubbletext1.style.backgroundImage = "url('../images/thoughtBubble.jpg')";
       bubbletext1.textContent = "Oh yeah? Well what are we waiting for? Let's sign up!";
       pointInConversation = 3;
     }
@@ -26,11 +26,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   leftSoldier.addEventListener("mouseleave", () => {
     bubbletext1.textContent = "";
+    leftBubble.style.backgroundImage = "";
   });
 
   rightSoldier.addEventListener("mouseenter", () => {
     if (pointInConversation === 1) {
-      bubbletext2.style.backgroundImage = "url('../images/thoughtBubble.jpg')";
       bubbletext2.textContent = "Didn't you hear? Mike and Marcel designed an incredible app to help us remember!";
       pointInConversation = 2;
     }
@@ -38,5 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   rightSoldier.addEventListener("mouseleave", () => {
     bubbletext2.textContent = "";
+    rightBubble.style.backgroundImage = "";
   });
+
+
 });
